c7027e58830ce17c7278555767d9abc763e99e0b
Gleb Rogachev <glebrogachyov@yandex.ru>
2023-01-28 14:41:25 +0300

    optimized admin json file

--- tools/Admin_class.py
+++ tools/Admin_class.py
@@ -4,26 +4,34 @@ import os
 
 class Admin:
     def __init__(self):
-        self.file_path = "settings/admins.json"
+        self.admin_file = "settings/admin_list.json"
+        self.manager_file = "settings/admin_manager.json"
         self.admin_list = []
         self.manager = None
         self.load_from_json()
 
     def load_from_json(self):
-        if "admins.json" not in os.listdir("settings"):
+        if "admin_list.json" not in os.listdir("settings"):
             exit("Ошибка. Файл 'admins.json' отсутствует в папке settings")
+
         try:
-            with open(self.file_path, "r") as fp:
+            with open(self.admin_file, "r") as fp:
                 data = json.load(fp)
-            self.admin_list = data.get("admin_id_list")
-            self.manager = data.get("manager_id")
-            return "Список администраторов прочитан успешно."
+                self.admin_list = data.get("admin_list")
+            if "admin_manager.json" in os.listdir("settings"):
+                with open(self.manager_file, "r") as fp:
+                    data = json.load(fp)
+                    self.manager = data.get("manager_id")
+
         except json.JSONDecodeError:
-            exit("Ошибка во время чтения файла 'settings/admins.json'.")
+            exit("Ошибка во время чтения файла 'settings/admin_list.json' или 'settings/admin_manager.json'.")
 
     def write_to_json(self):
-        data = {"admin_id_list": self.admin_list, "manager_id": self.manager}
-        with open(self.file_path, "w") as fp:
+        # with open(self.admin_file, "w") as fp:        # Раскомментировать если понадобится создать файл списка админов
+        #     data = {"admin_list": self.admin_list}
+        #     json.dump(data, fp)
+        with open(self.manager_file, "w") as fp:
+            data = {"manager_id": self.manager}
             json.dump(data, fp)
 
     def change_manager(self, index_in_admin_list: int):
